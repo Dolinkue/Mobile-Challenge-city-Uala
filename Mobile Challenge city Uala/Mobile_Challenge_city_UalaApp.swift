d416882acba0bc66103407e31805f2c14f63b58a
Nicolas Dolinkue <dolinkue_n@hotmail.com>
2025-03-20 19:26:59 -0300

    Initial Commit

---
+++ Mobile Challenge city Uala/Mobile_Challenge_city_UalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_Challenge_city_UalaApp.swift
+//  Mobile Challenge city Uala
+//
+//  Created by Nicolas Dolinkue on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_Challenge_city_UalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mobile Challenge city UalaTests/Mobile_Challenge_city_UalaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_Challenge_city_UalaTests.swift
+//  Mobile Challenge city UalaTests
+//
+//  Created by Nicolas Dolinkue on 20/03/2025.
+//
+
+import Testing
+@testable import Mobile_Challenge_city_Uala
+
+struct Mobile_Challenge_city_UalaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
